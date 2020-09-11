ad8b8e5a4c383c6ccd6c64a9d0de91918fbec1f4
Kristin Beltz <kikkib@Kikki-MacbookPro.local>
2020-09-11 08:27:18 -0500

    Update template engine and add basic views

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
+var expressHandlebars = require('express-handlebars');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +18,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+  res.render('home')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/menu', (req, res) => {
+  res.render('menu')
+});
+app.get('/contact', (req, res) => {
+  res.render('contact')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
